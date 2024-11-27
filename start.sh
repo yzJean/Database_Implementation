613b668666141d5595a640a875f2ee2032aeb028
jean.lin <jeanlin1203@gmail.com>
2024-11-26 22:29:47 -0500

    Modify start.sh

--- start.sh
+++ start.sh
@@ -5,10 +5,18 @@ build_dir="./build/"
 if [ -z $build_dir ]; then
     mkdir -p $build_dir
 fi
-javac Database.java -d $build_dir 
+javac *.java -d $build_dir 
 echo "Built successfully"
 
-main_program="Database"
 cd $build_dir
+echo ""
+echo "Run Database1: "
+main_program="Database1"
 java $main_program
-echo "End of program"
+echo "End of Database1"
+
+echo ""
+echo "Run Database2: "
+main_program="Database2"
+java $main_program
+echo "End of Database2"
